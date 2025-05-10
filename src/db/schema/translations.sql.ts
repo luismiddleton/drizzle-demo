9af5752ea8e1f7457264211a2f7714b9e4745060
Luis Middleton <luizinho3@gmail.com>
2025-05-10 17:15:30 +1000

    composite pk for translations

--- src/db/schema/translations.sql.ts
+++ src/db/schema/translations.sql.ts
@@ -1,9 +1,13 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, primaryKey, varchar } from "drizzle-orm/pg-core";
 import { timestamps } from "./column.helpers";
 
-export const languageTable = pgTable("translations", {
-  languageId: integer().primaryKey(),
-  textContentId: integer().primaryKey(),
-  translation: varchar({ length: 200 }).notNull(),
-  ...timestamps
-});
+export const languageTable = pgTable(
+  "translations",
+  {
+    languageId: integer(),
+    textContentId: integer(),
+    translation: varchar({ length: 200 }).notNull(),
+    ...timestamps,
+  },
+  (table) => [primaryKey({ columns: [table.languageId, table.textContentId] })]
+);

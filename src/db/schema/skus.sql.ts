bd862539ff3a11c07fbde215efbf89ed1af4f2d4
Luis Middleton <luizinho3@gmail.com>
2025-05-10 17:04:12 +1000

    fix table name for sku

--- src/db/schema/skus.sql.ts
+++ src/db/schema/skus.sql.ts
@@ -1,7 +1,7 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 import { timestamps } from "./column.helpers";
 
-export const skuTable = pgTable("skuTable", {
+export const skuTable = pgTable("skus", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   sku: varchar({ length: 10 }),
   quantity: integer().notNull().default(0),
